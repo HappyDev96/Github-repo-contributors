58b6ba3fec1fdbaed9d5f4b72da55e039e35b3f3
Happy Dev <qmhappydev@outlook.com>
2018-10-11 15:51:13 +0300

    Sort data by amount of public repos, contributions, followers, gists

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import avartar from '../img/identicon.png';
 import './Content.css';
 
 import contributors from '../mockup.json';
 
 class Content extends Component {
+
   render() {
     return (
       <div className="Content">
         <div className="c-content">
             <ul>
             {
-                contributors.map(function(contributor){
-                    return <li>{contributor.name} - {contributor.contributions} - {contributor.followers} - {contributor.pubRepos} - {contributor.gists} </li>;
+                contributors.sort(
+                  (a,b) => (b.contributions + b.followers + b.pubRepos + b.gists ) - (a.contributions + a.followers + a.pubRepos + a.gists) 
+                ).map(function(contributor){
+                    return <li><img src={avartar} />
+                    {contributor.name} 
+                    - {contributor.contributions}
+                    - {contributor.followers}
+                    - {contributor.pubRepos}
+                    - {contributor.gists}
+                    </li>;
                 })
             }
             </ul>
